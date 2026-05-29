e671bafde6299ecf0cc2b715593c949bc9c80ffd
116_Lv <lbw0109@gmail.com>
2026-05-29 17:31:55 +0900

    feat: Initial Commit

---
+++ src/main/java/com/team11/jojopay/JojoPayApplication.java
@@ -0,0 +1,19 @@
+package com.team11.jojopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableJpaAuditing
+@EnableScheduling // 도전 3단계의 스케쥴러 동작용
+@EnableAsync      // 포트원 응답 주고 받을때 최적화용
+@SpringBootApplication
+public class JojoPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JojoPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team11/jojopay/JojoPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team11.jojopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JojoPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
