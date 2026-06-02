2d0c8fa2c9813b5ba9720ccb3b3e232c9aac3c01
116_Lv <lbw0109@gmail.com>
2026-06-02 20:16:10 +0900

    feat: 응답에 필요한 ResponseDto 추가

---
+++ src/main/java/com/team11/jojopay/domain/point/dto/response/PointBalanceResponse.java
@@ -0,0 +1,12 @@
+package com.team11.jojopay.domain.point.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class PointBalanceResponse {
+
+    private final Long pointBalance;
+
+}
