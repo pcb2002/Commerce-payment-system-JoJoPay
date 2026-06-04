f3091d7d4ade1e932346cb7d7867ff773bdc78af
kera9960 <kera9960@gmail.com>
2026-06-04 16:33:44 +0900

    feat: 구독 관련 에러코드 추가

--- src/main/java/com/team11/jojopay/common/exception/ErrorCode.java
+++ src/main/java/com/team11/jojopay/common/exception/ErrorCode.java
@@ -56,6 +56,7 @@ public enum ErrorCode {
     ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER_NOT_FOUND", "주문 내역이 존재하지 않습니다."),
     WALLET_NOT_FOUND(HttpStatus.NOT_FOUND, "WALLET_NOT_FOUND", "회원의 페이머니 지갑을 찾을 수 없습니다."),
     CART_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "CART_ITEM_NOT_FOUND", "장바구니 항목을 찾을 수 없습니다."),
+    SUBSCRIPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "SUBSCRIPTION_NOT_FOUND", "구독 정보를 찾을 수 없습니다."),
 
 
     // =========================================================================
@@ -66,6 +67,8 @@ public enum ErrorCode {
     PRODUCT_DISCONTINUED(HttpStatus.CONFLICT, "PRODUCT_DISCONTINUED", "판매가 중단(단종)된 전자제품은 주문할 수 없습니다."),
     PRODUCT_ALREADY_DELETED(HttpStatus.CONFLICT, "PRODUCT_ALREADY_DELETED", "이미 삭제 처리된 상품입니다."),
     MEMBER_ALREADY_DELETED(HttpStatus.CONFLICT, "MEMBER_ALREADY_DELETED", "이미 탈퇴 또는 삭제 처리된 회원입니다."),
+    ALREADY_ACTIVE_SUBSCRIPTION(HttpStatus.CONFLICT, "ALREADY_ACTIVE_SUBSCRIPTION", "이미 활성 구독이 존재합니다."),
+    NO_ACTIVE_SUBSCRIPTION(HttpStatus.CONFLICT, "NO_ACTIVE_SUBSCRIPTION", "해지할 활성 구독이 존재하지 않습니다."),
 
     // 조조페이머니 핵심 비즈니스 정합성 정책
     INSUFFICIENT_BALANCE(HttpStatus.CONFLICT, "INSUFFICIENT_BALANCE", "페이머니 잔액이 부족하여 결제할 수 없습니다."),
